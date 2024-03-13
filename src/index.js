f63eb8130109f2c8241ef79be52e56704fd889a5
DaisiSegun <daissegun096@gmail.com>
2024-03-13 18:23:40 +0100

    test 2
    lets go

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <HashRouter basename="/">
+        <App />
+      </HashRouter>
     </HelmetProvider>
   </React.StrictMode>,
   document.getElementById('root')
